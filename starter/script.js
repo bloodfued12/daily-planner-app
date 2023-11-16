61f28acb159fce80c923500f0d554bb49c4384bd
bloodfued12 <benjamin.smith.ep@gmail.com>
2023-11-16 09:20:36 +0000

    Added changes to logic

--- starter/script.js
+++ starter/script.js
@@ -1,15 +1,18 @@
 // Declared .container as a jquery element so that it can be accessed to insert the timeblocks
 var container = $(".container");
-
-$.each(function () {
-    var timeEl = $("<div>");
+var timeEl = $("<div>");
     var titleEl = $("<h3>");
     timeEl.append(titleEl);
-    var time = $.each( function () {
-        var count = $("count");
-        count = count++;
-    }) 
-    titleEl.text(count);
-    container.append(timeEl);
-});
+    
+// $.each(function () {
+//     var timeEl = $("<div>");
+//     var titleEl = $("<h3>");
+//     timeEl.append(titleEl);
+//     var time = $.each( function () {
+//         var count = $("count");
+//         count = count++;
+//     }) 
+//     titleEl.text(count);
+//     container.append(timeEl);
+// });
 
